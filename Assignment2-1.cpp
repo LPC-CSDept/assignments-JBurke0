d01bd3a1871a010864924ab5bca57ab12d3c6a57
Your Name <you@example.com>
2022-02-07 20:14:48 -0800

    Code2-13

--- Assignment2-1.cpp
+++ Assignment2-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout<<"We have made a long journey of 4276 miles." << endl;
-    cout<< "Our checking account balance is -20." << endl; 
-    cout<<"About 189000 days ago Columbus stood on this spot." << endl; 
+    cout<<"We have made a long journey of 4276 miles." \n;
+    cout<< "Our checking account balance is -20." \n; 
+    cout<<"About 189000 days ago Columbus stood on this spot."<<\n;
 }
---
+++ Code2-13.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "A";
+    char A=65;
+    cout << "B";
+    char B=66;
+}
