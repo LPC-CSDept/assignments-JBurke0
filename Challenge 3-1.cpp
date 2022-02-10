3fb88d8f758ea891c7441211c01f17053c0459d7
Your Name <you@example.com>
2022-02-10 11:01:48 -0800

    Challenge 3-1

--- Challenge 3-1.cpp
+++ Challenge 3-1.cpp
@@ -3,9 +3,23 @@
 using namespace std;
 int main {
 
-    int a=15;
-    int b=12;
-    int c=9; 
+    int class A;
+     int classB;
+    int classC; 
 
+int priceA = 15;
+int priceB = 12;
+int priceC = 9; 
 
+cout<< "Enter number of tickets sold for Class A";
+cin >> class A; 
+
+cout<< "Enter number of tickets sold for Class B";
+cin >> classB; 
+
+cout<< "Enter number of tickets sold for Class C";
+cin >> classC; 
+
+total=( class A+priceA)+(classB+priceB)+(classC+priceC);
+cout << "Total price for ticket sold" << total << endl;
 }
