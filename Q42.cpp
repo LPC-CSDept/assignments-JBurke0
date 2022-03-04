318d501f692bc3bdde81ece04604294ac7a6db5c
Your Name <you@example.com>
2022-03-03 20:30:17 -0800

    Q42

--- Q42.cpp
+++ Q42.cpp
@@ -8,12 +8,9 @@ int main()
   int i=0;
   double x, weight,cost,miles;
   cout<<"Enter number of miles "<<endl;
-  cin>>miles;
-    cout << "Enter the weight of the package "<<endl;
-cin>> weight;
-if (weight>0 && weight>20) 
-if (miles>10&&3000)
-{
+   cin>>miles;
+   cout<< "Enter the weight of the package: "<<endl;
+   cin>>weight;
    i=miles/500;
    x=miles/500;
    if(x>0){
@@ -21,17 +18,13 @@ if (miles>10&&3000)
    }
    if(weight<=2){
        cost=i*1.10;
-   }else 
-   if(weight>2 && weight<=6){
+   }else if(weight>2 && weight<=6){
        cost=i*2.20;
-   }else
-    if(weight>6 && weight<=10){
+   }else if(weight>6 && weight<=10){
        cost=i*3.70;
-   }else 
-   if(weight>10 && weight<=20){
+   }else if(weight>10 && weight<=20){
        cost=i*4.80;
    }
-   cout << "Cost of Total Cost of Shipping: "<< cost;
+   cout<<"Total Cost of Shipping" <<cost;
    return 0;
-}
 }
