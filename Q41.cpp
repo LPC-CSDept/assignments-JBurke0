8e7b28da0b3dc2dc4a1b35dc1c8ce25c4fabaf7b
Your Name <you@example.com>
2022-03-03 19:09:03 -0800

    Q41

--- Q41.cpp
+++ Q41.cpp
@@ -6,5 +6,32 @@ using namespace std;
 
 int main()
 {
-  
+  	int choice;
+
+	cout << "Menu \n";
+	cout << "1.Calculate Area of Circle"<< endl;
+	cout << "2.Calculate Area of Rectangle"<< endl;
+	cout << "3.Calculate Area of Triangle"<< endl;
+	cout << "Quit";
+	cin >> choice;
+
+    switch(choice)
+    double width,height,radius,length,base;
+     {
+		case 1:	
+        	
+			cout << "Area of Circle" << endl;
+			break;
+		case 2:		
+			cout << "Area of Rectangle" << endl;
+			break;
+		case 3:		
+			cout << "Area of Triangle" << endl;
+			break;
+		case 4:		
+			cout << "You chose menu 4\n";
+			break;
+		default:	
+			cout << "Wrong menu item \n";
+	}
 }
