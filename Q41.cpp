0e4224e03680399dd9565f4489a653c42d91c34e
Your Name <you@example.com>
2022-03-03 19:53:27 -0800

    Q41

--- Q41.cpp
+++ Q41.cpp
@@ -17,31 +17,46 @@ int main()
 	cin >> choice;
 
 double width,height,radius,length,base;
+
 double AreaCircle,AreaRectangle,AreaTriangle;
     switch(choice)
 
      {
 		case 1:
+
+        if (radius>0)
+        {
             cout << "Enter radius"<<endl;
             cin>>radius;
             AreaCircle=(pow(radius,2)*3.14159);
 			cout << "Area of Circle"<<AreaCircle<< endl;
+        }
 			break;
 		case 2:
+        if (length>0 && width>0)
+        {
             cout<<"Enter length"<<endl;
             cin>>length;
             cout<<"Enter width"<<endl;
             cin>>width;
             AreaRectangle=length*width;
 			cout << "Area of Rectangle" << AreaRectangle << endl;
+        }
 			break;
 		case 3:
+        if (base>0 && height>0)
+        {
             cout<<"Enter base"<<endl;
             cin>>base;
             cout<<"Enter height"<<endl;
             cin>>height;
             AreaTriangle=base*height*0.5;
 			cout << "Area of Triangle"<< AreaTriangle << endl;
+        }
+        else
+        {
+        	cout<<"Values must be positive"<<endl;
+        }
 			break;
 		case 4:
 			cout << "You chose choice 4";
