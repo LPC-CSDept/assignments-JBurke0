c9fa48f28c8eccb9da52757b8012c509e2097ef1
Your Name <you@example.com>
2022-03-03 20:08:49 -0800

    Q42

--- Q42.cpp
+++ Q42.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime> 
+#include <cstdlib>
+#include <ctime>
 #include<iomanip>
 using namespace std;
 int main()
@@ -10,14 +10,24 @@ int main()
   cout<<"Enter number of miles "<<endl;
   cin>>miles;
     cout << "Enter the weight of the package "<<endl;
-cin>> weight
+cin>> weight;
    i=miles/500;
-   x=miles%500;
+   x=miles/500;
    if(x>0){
-       i=i+1;
-  
+       i=i++;
+   }
+   if(weight<=2){
+       cost=i*1.10;
+   }else 
+   if(weight>2 && weight<=6){
+       cost=i*2.20;
+   }else
+    if(weight>6 && weight<=10){
+       cost=i*3.70;
+   }else 
+   if(weight>10 && weight<=20){
+       cost=i*4.80;
    }
    cout << "Cost of Shipping: "<< cost;
    return 0;
-}
 }
