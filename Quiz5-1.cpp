90837acf144634c5741c22b74049c13b36e54358
Your Name <you@example.com>
2022-03-22 19:18:47 -0700

    5-2

--- Quiz5-1.cpp
+++ Quiz5-1.cpp
@@ -1,41 +1,22 @@
 #include <iostream>
-using namespace std;
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
 int main()
 {
-	  int num1, num2,num3,num4,num5;
-		srand(time(0));
+	const int 	N = 5;
+	int 	rdnum;
+	int 	min;
 
-		num1 = rand() % 100 ;
-		cout << num1 << endl;
+	srand(time(0));
 
-		num2 = rand() % 100 ;
-		cout << num2 << endl;
+	for(int i=0;i<N;i++){
+		rdnum = rand() % 100 ;
+		cout << rdnum << "\t";
+		if ((i==0) || ( min > rdnum))
+			min = rdnum;
+	}
+	cout << "Minimum : " << min << endl;
 
-
-		num3 = rand() % 100 ;
-		cout << num3 << endl;
-
-		num4 = rand() % 100 ;
-		cout << num4 << endl;
-
-
-		num5 = rand() % 100 ;
-		cout << num5 << endl;
-
-
-			int 	min=0;
-			int i;
-num1,num2,num3,num4,num5=i;
-			for(int i=0;i< 5; i++)
-			{
-
-				if ( (i == 0) || (min > num1)or(min>num2)or(min>num3)or(min>num4)or(min>num5) )
-
-min =i;
-
-			cout << " Minimum value of input is " << min << endl;
-			}
 }
--- Quiz5-2.cpp
+++ Quiz5-2.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include<iomanip>
-
 using namespace std;
+
 int main()
 {
-	double base,exponent;
-	cout<<"Enter a base"<<endl;
-	cin>>base;
-	cout<<"Enter a exponent"<<endl;
-	cin>>exponent;
-
+float base,power,result=1;
+cout<<"Enter the base"<<endl;
+cin>>base;
+cout<<"Enter to the  power of"<<endl;
+cin>>power;
+while(power!=0&&power<10)
 {
-for (base>0 && base<10 && exponent>=1 && exponent<=10, base*=exponent)
-cout base*=exponent<<"\t";
-cout<< endl;
-	
-
+	result*=base;
+	--power;
+}
+cout<<result;
 }
