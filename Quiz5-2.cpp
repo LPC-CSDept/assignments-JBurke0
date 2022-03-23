2d29b10c30645d4da8ddb91829f310aeffefe911
Your Name <you@example.com>
2022-03-22 17:21:01 -0700

    5-2

---
+++ Quiz5-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+    cout<<"Enter base"<<endl;
+    cin>>base;
+    cout<<"Enter to the power of"
+    cin>>power;
+    int i;
+    for (i=2;i)
+}
