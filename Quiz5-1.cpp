7cac28d27ce71571c15c4bd3c487cd9e79826f7d
Your Name <you@example.com>
2022-03-22 18:23:17 -0700

    5-1

--- Quiz5-1.cpp
+++ Quiz5-1.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 using namespace std;
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-    int num1, num2,num3,num4,num5;
-	srand(time(0));
-	
-	num1 = rand() % 100 ;  
-	cout << num1 << endl;
+	  int num1, num2,num3,num4,num5;
+		srand(time(0));
 
-	num2 = rand() % 100 ;   
-	cout << num2 << endl;
+		num1 = rand() % 100 ;
+		cout << num1 << endl;
 
-  	
-	num3 = rand() % 100 ;  
-	cout << num3 << endl;
+		num2 = rand() % 100 ;
+		cout << num2 << endl;
 
-	num4 = rand() % 100 ;  
-	cout << num4 << endl;
-   
-	
-	num5 = rand() % 100 ; 
-	cout << num5 << endl;
 
-	
+		num3 = rand() % 100 ;
+		cout << num3 << endl;
 
-	int 	min=0;
+		num4 = rand() % 100 ;
+		cout << num4 << endl;
 
-		{
-		cout << "Enter the user input: ";
-		cin >> input;
-		if ( (i == 0) || (min > input) )
-			min = input;
-	}
-	cout << " Minimum value of input is " << min << endl;
+
+		num5 = rand() % 100 ;
+		cout << num5 << endl;
+
+
+			int 	min=0;
+
+			for(int i=0;i< 5; i++)
+			{
+
+				if ( (i == 0) || (min > num1,num2,num3,num4,num5) )
+					min = num1,num2,num3,num4,num5;
+			}
+			cout << " Minimum value of input is " << min << endl;
+}
 }
