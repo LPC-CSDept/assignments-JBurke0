efabcff2b5998918954cd2865b84300061662631
Your Name <you@example.com>
2022-03-22 17:14:38 -0700

    Quiz5-3

---
+++ Quiz5-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+int flag ;
+
+for(int current_num=1;	current_num<=100; current_num++)
+{
+	flag = 0;
+	for(int i=2;i<=current_num/2;i++)
+	{
+		if ( current_num % i == 0)
+		{
+			flag = 1;
+			break;
+		}
+
+	}
+	if ( flag )
+		cout << current_num << " Is not a prime number " << endl;
+	else
+		cout << current_num << "Is a prime number " << endl;
+}
+}
