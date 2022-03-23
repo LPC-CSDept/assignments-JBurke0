c949e301769c30bb9efc9bc6d50be5b72f0c58c8
Your Name <you@example.com>
2022-03-22 18:55:45 -0700

    5-1

--- Quiz5-2.cpp
+++ Quiz5-2.cpp
@@ -11,9 +11,11 @@ int main()
 	cin>>base;
 	cout<<"Enter a exponent"<<endl;
 	cin>>exponent;
-if base>0 && base<10 && exponent>=1 && exponent<=10
+
 {
-for (
+for (base>0 && base<10 && exponent>=1 && exponent<=10, base*=exponent)
+cout base*=exponent<<"\t";
+cout<< endl;
 	
 
 }
