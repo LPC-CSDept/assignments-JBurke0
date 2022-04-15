6a8038ffa545359b7aa5d38133e1c06d5f58a75d
Your Name <you@example.com>
2022-04-15 15:28:34 -0700

    Quiz6-1

--- .vscode/Quiz6-1.cpp
+++ .vscode/Quiz6-1.cpp
@@ -2,33 +2,48 @@
 #include <cstdlib>
 #include <ctime>
 #include<iomanip>
+
 using namespace std;
 
-void swapTwoValues(int n1, int n2)
-void swapTwoValues(float f1, float f2)
+void swapTwoValues(int*n1, int*n2);
+void swapTwoValues(float*f1, float*f2);
+
 
 int main() {
-int a[5]=(1,2,3,4,5);
-swapTwoValues(a[2]&&a[3]);
-for(int i=0;i<5;i++){
-    cout<<"%d"<<a[i]<<endl;
+	
 
-}
-float b[5]=(5,4,3,2,1);
-swapTwoValues(b[2]&&b[3]);
-for(int i=0;i<5;i++){
-    cout<<"%f"<<b[i]<<endl;
+        int a[5] = {1,2,3,4,5};
+        swapTwoValues(&a[2], &a[3]);
 
+        for(int i=0;i<5;i++)
+        {
+                cout<<a[i]<<endl;
+        }
+
+
+        float b[5] = {5,4,3,2,1};
+
+        swapTwoValues(&b[2],&b[3]);
+
+                for(int i=0;i<5;i++)
+                {
+                cout<<b[i]<<endl;
+        }
+
+        return 1;
 }
-return 1;
+
+void swapTwoValues(int n1, int n2) {
+
+        int var = n1;
+        n1 = n2;
+        n2 = var;
 }
-void swapTwoValues(int n1, int n2)
-int n1;
-n1=n2;
-int n2;
-}
-void swapTwoValues(float f1, float f2)
-float n1;
-n1=n2;
-float n2;
+
+void swapTwoValues(float f1, float f2) {
+
+        float var = f1;
+        f1 = f2;
+        f2 = var;
+
 }
