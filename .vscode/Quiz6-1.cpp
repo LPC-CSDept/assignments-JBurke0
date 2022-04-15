5c6d6d8af1e79763b3d78c31dc4d02ce93e28ed5
Your Name <you@example.com>
2022-04-15 13:42:23 -0700

    Quiz6-1

--- .vscode/Quiz6-1.cpp
+++ .vscode/Quiz6-1.cpp
@@ -8,5 +8,10 @@ void swapTwoValues(int n1, int n2)
 void swapTwoValues(float f1, float f2)
 
 int main {
-
+int a[5]=(1,2,3,4,5);
+swapTwoValues(a[2]&a[3]);
+for(int i=0;i<5;i++){
+    cout<<"%d",a[i]<<endl;
+    
+}
 }
