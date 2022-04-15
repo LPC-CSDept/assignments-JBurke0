c796a1f75ac46873f263f5e74df6ae9fab134e26
Your Name <you@example.com>
2022-04-15 13:27:39 -0700

    Quiz6-1

---
+++ .vscode/Quiz6-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+using namespace std;
+
+void swapTwoValues(int n1, int n2)
+void swapTwoValues(float f1, float f2)
+
+int main {
+
+}
