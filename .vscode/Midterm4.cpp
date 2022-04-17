769622dac86519a6958a40eecb9d7deaac2a9927
Your Name <you@example.com>
2022-04-17 16:57:29 -0700

    Midterm4

--- .vscode/Midterm4.cpp
+++ .vscode/Midterm4.cpp
@@ -8,3 +8,13 @@ int getRdnum(){
    srand ( time(0) );
    return rand() % 101;
 }
+
+}
+int getRdnum(int n1){
+   srand ( time(0) );
+   return rand() % (101-n1)+n1;
+}
+int getRdnum(int n1,int n2){
+   srand ( time(0) );
+   return rand() % (n2-n1)+n1;
+}
