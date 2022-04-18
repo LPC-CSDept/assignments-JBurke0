461130b8afac1e265e1fde66406fcfcfd2ef84bc
Your Name <you@example.com>
2022-04-17 17:00:24 -0700

    Midterm4

--- .vscode/Midterm4.cpp
+++ .vscode/Midterm4.cpp
@@ -4,11 +4,10 @@
 #include<iomanip>
 
 using namespace std;
+
 int getRdnum(){
    srand ( time(0) );
    return rand() % 101;
-}
-
 }
 int getRdnum(int n1){
    srand ( time(0) );
@@ -17,4 +16,10 @@ int getRdnum(int n1){
 int getRdnum(int n1,int n2){
    srand ( time(0) );
    return rand() % (n2-n1)+n1;
+}
+int main(){
+   cout<<getRdnum()<<endl;
+   cout<<getRdnum(50)<<endl;
+   cout<<getRdnum(1,99)<<endl;
+
 }
