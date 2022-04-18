9b94c9cf396775244bf39ce09058000db1962027
Your Name <you@example.com>
2022-04-17 17:14:48 -0700

    Midterm2

---
+++ .vscode/Midterm2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	ifstream 	ifs;
+	int 		numofStudent;
+	string 		studentName;
+	double 		score1, score2;
+	double 		sum, avg;
+	double 		tsum, tavg;
+	
+	ifs.open("students.txt");
+	if (!ifs)
+	{
+		cerr << "File Open Error\n";
+		exit(0);
+	}
+
+	ifs >> numofStudent;
+
+	tsum = 0;
+	for(int i=0; i<numofStudent; i++){
+		ifs >> studentName >> score1 >> score2;
+		sum = score1 + score2;
+		avg = sum / 2;
+		tsum += sum;
+		cout << studentName << "\t" << score1 << "\t" << score2 << endl;
+		cout << "Sum of Score:\t" << sum << "\t" << "Average Score:\t"<< avg << endl;
+	}
+	tavg = tsum / numofStudent;
+	cout << "Toal Sum of Score:\t" << tsum << "\t" << "Total Average Score:\t"<< tavg << endl;
+	
+
+}
