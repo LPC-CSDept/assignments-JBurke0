79e795e744cfd9f5d18bf6370d4891d712e8f250
Your Name <you@example.com>
2022-04-17 18:31:57 -0700

    Midterm3

--- .vscode/Midterm3.cpp
+++ .vscode/Midterm3.cpp
@@ -12,3 +12,16 @@ int getRdnum()
     srand(time(0));
     cout<<rand()%50+1;
 }
+
+
+int isGreater(int n)
+
+{
+   for (int i = 2; i < n / 2; i++)
+
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
