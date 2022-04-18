3a8a47dd1007b20d6e8f1a2d48eef0325bf116a5
Your Name <you@example.com>
2022-04-17 17:47:32 -0700

    Midterm5

--- .vscode/Midterm5.cpp
+++ .vscode/Midterm5.cpp
@@ -4,3 +4,27 @@
 #include<iomanip>
 
 using namespace std;
+int inputX,inputY;
+int getInput()
+{
+cout<<"User input variable x"<<endl;
+cin>>inputX;
+cout<<"User input variable y"<<endl;
+cin>>inputY;
+}
+
+int isSame()
+{
+    if inputX==inputY{
+        cout<<"Inputs must be different numbers"<<endl;
+        cout<<"Program will now exit, Bye!"<<endl;
+        exit(0);
+    }
+}
+
+int isDivision()
+{
+if inputX > inputY {
+    
+}
+}
