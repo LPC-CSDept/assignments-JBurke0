00300307107f5952e2bbc1a1e508444fafcd2eb9
Your Name <you@example.com>
2022-05-10 13:44:31 -0700

    Quiz7-2

---
+++ Quiz7-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+#include <fstream>
+
+using namespace std;
+void printtriangle(int numbers[][3], int);
+int main()
+{
+    const int SIZE = 3;
+    int numbers[SIZE][SIZE] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
+    
+    printtriangle(numbers, SIZE);
+}
+void printtriangle(int num[][3], int size)
+{
+   
+}
