3d3cf22825075b382ebaeafb9e79b530e586d96b
Your Name <you@example.com>
2022-05-10 14:02:08 -0700

    Quiz7-3

---
+++ Quiz7-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include<iomanip>
+#include <fstream>
+
+using namespace std;
+
+const int N=10;
+int number[N]={10,15,0,-25,19,20,25,30,45,20};
