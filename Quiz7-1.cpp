97b0aa0c3c568b417cea2d61c1787000fbc88f4d
Your Name <you@example.com>
2022-05-10 17:10:02 -0700

    Quiz7-1

--- Quiz7-1.cpp
+++ Quiz7-1.cpp
@@ -14,13 +14,16 @@ for (int i=0;i<N,i++){
         if(abs(number[i]-number[i--])<10){
             cout<<number[i]<<endl;
         }
-    }else if(i==N-1 && i>0){if abs(number[i]-number[i--]<10)}{
+    }else if(i==N-1 && i>0){
+    {if (abs(number[i]-number[i--]<10)){
         cout<<"Index:"<<number[i]<<endl;
     }
-    else if(  |number[i] - number[i-1]|  < 10  &&
- |number[i] - number[i+1]|  < 10  )
+    else if { ( abs(number[i] - number[i-1]  < 10)&&
+ (number[i] - number[i+1]|  < 10  ))
  cout<<number[i]<<endl;
 }
 return 1;
+    }
+         }
 }
-      
+} 
