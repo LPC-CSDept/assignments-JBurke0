cb3c22d4f23d253b1113cd075c85e618145aaf1c
Your Name <you@example.com>
2022-05-12 13:23:22 -0700

    Chapter4

---
+++ Assignment4-1-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+int main()
+{
+ int i=0;
+  double x, weight,cost,miles;
+  cout<<"Enter number of miles "<<endl;
+   cin>>miles;
+   if (miles>10&&miles<3000)
+   cout<< "Enter the weight of the package: "<<endl;
+   cin>>weight;
+   if (weight>0 && weight<20)
+   i=miles/500;
+   x=miles/500;
+   if(x>0){
+       i=i++;
+   }
+   if(weight<=2){
+       cost=i*1.10;
+   }else if(weight>2 && weight<=6){
+       cost=i*2.20;
+   }else if(weight>6 && weight<=10){
+       cost=i*3.70;
+   }else if(weight>10 && weight<=20){
+       cost=i*4.80;
+   }
+   cout<<"Total Cost of Shipping" <<cost;
+   return 0;
+}
