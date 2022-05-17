976ef88c67f913cb367cddde5520f7044c5c3cb1
Your Name <you@example.com>
2022-05-16 17:27:26 -0700

    Lab5-1

---
+++ Assignment 5-7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    float base,power,result=1;
+cout<<"Enter the base"<<endl;
+cin>>base;
+cout<<"Enter to the  power of"<<endl;
+cin>>power;
+while(power!=0&&power<10)
+{
+	result*=base;
+	--power;
+}
+cout<<result;
+}
---
+++ Assignment 5-8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+  double x = 1.0;
+for (int n = 0; n <= 100; n++)
+{
+    n=pow(n,2);
+    cout<<n
+}
+return 0;
+}
