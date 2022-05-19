07ee140e980bd285eb86d09336bb2f03205ace3e
Your Name <you@example.com>
2022-05-19 15:41:57 -0700

    Quiz9

--- .vscode/Quiz9.cpp
+++ .vscode/Quiz9.cpp
@@ -4,19 +4,33 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-
-
+using namespace std; 
+const	int 	NUMCOURSE=2;
 struct Employee
 {
 	int		id;
 	string	fname;
     string lname;
-	int 	salary;	
+	int 	salary[NUMCOURSE];	
 	int		deptName;
 	int		date;
 };
 
+void printEmployee(Employee s);
 int main()
 {
-
+	Employee s;
+	ifstream  ifs;
+	ifs.open("Workers.txt");
+	if (ifs) 
+	{
+		while (ifs >> s.id >> s.lname >> s.fname>>s.deptName >> s.date )
+		{
+			s.sum = 0;
+			for(int i=0; i<NUMCOURSE; i++){
+				ifs >> s.salary[i] ;
+					
+			printEmployee(s);
+		}
+	}
 }
