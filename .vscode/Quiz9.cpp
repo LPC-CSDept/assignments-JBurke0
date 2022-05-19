247a363b8204cd814d4de11d485dddc8fd7a90f9
Your Name <you@example.com>
2022-05-19 15:28:55 -0700

    Quiz9

--- .vscode/Quiz9.cpp
+++ .vscode/Quiz9.cpp
@@ -5,4 +5,18 @@
 #include <algorithm>
 #include <iterator>
 
+
+struct Employee
+{
+	int		id;
+	string	fname;
+    string lname;
+	int 	salary;	
+	int		deptName;
+	int		date;
+};
+
 int main()
+{
+
+}
