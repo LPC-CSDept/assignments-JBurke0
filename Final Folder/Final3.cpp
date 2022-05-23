2515dea0278321426f0256772456956c3c0b83a4
Your Name <you@example.com>
2022-05-23 14:38:47 -0700

    Final

--- Final Folder/Final3.cpp
+++ Final Folder/Final3.cpp
@@ -9,11 +9,19 @@ using namespace std;
 
 int main()
 {
+    struct[numbers]
+    {
     const int SIZE1=5;
 const int SIZE2=10;
 int numbers1[SIZE1] = {2, 3, 5, 9, 10};
 int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+    }
+    
 
+	cout << " Total numbers in size 1" << SIZE1.studentID << endl;
+	cout << "Numbers in size 1 " << SIZE2.name << endl; 
+	cout << "Total Numbers in size 2 " << SIZE1.yearInSchool << endl; 
+	cout << "Numbers in size 2" << SIZE2. << endl; 
 
 	
 }
