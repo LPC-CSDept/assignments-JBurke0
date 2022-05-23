de0d202034df6a2e55e35bb916dd9c36aaa75bf6
Your Name <you@example.com>
2022-05-23 14:48:33 -0700

    Final

--- Final Folder/Final3.cpp
+++ Final Folder/Final3.cpp
@@ -16,12 +16,13 @@ const int SIZE2=10;
 int numbers1[SIZE1] = {2, 3, 5, 9, 10};
 int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
     }
-    
+    numbers s1 ={2, 3, 5, 9, 10}; 
+    numbers s2={1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 
-	cout << " Total numbers in size 1" << SIZE1.studentID << endl;
-	cout << "Numbers in size 1 " << SIZE2.name << endl; 
-	cout << "Total Numbers in size 2 " << SIZE1.yearInSchool << endl; 
-	cout << "Numbers in size 2" << SIZE2. << endl; 
+	cout << " Total numbers in size 1" << s1.SIZE1 << endl;
+	cout << "Numbers in size 1 " << s2.SIZE2 << endl; 
+	cout << "Total Numbers in size 2 " << s1.[SIZE1] << endl; 
+	cout << "Numbers in size 2" << s2.[SIZE2] << endl; 
 
 	
 }
